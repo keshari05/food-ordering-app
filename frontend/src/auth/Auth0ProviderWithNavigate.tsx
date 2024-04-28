cfac45bc99d52fcaed44dd89a4c2c4b6adb78801
Shubham Keshari <shubhamkeshari005@gmail.com>
2024-04-28 16:58:27 +0530

    react query setup and create user api call

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,28 +1,38 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 
-type Props ={
-    children: React.ReactNode;
-}
+type Props = {
+  children: React.ReactNode;
+};
 
-const Auth0ProviderWithNavigate = ({children}: Props) => {
-    const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-    const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-    const redirectUrl = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const { createUser } = useCreateMyUser();
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  const redirectUrl = import.meta.env.VITE_AUTH0_CALLBACK_URL;
 
-    if(!domain || !clientId || !redirectUrl) {
-        throw new Error('Unable to initialise auth');
+  if (!domain || !clientId || !redirectUrl) {
+    throw new Error("Unable to initialise auth");
+  }
+
+  const onRedirectCallback = (appState?: AppState, user?: User) => {
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user?.sub, email: user?.email });
     }
+  };
 
-    const onRedirectCallback = (appState?: AppState, user?: User) => {
-        console.log(user, 'user');
-    }
-
-    return  (
-        <Auth0Provider domain={domain} clientId={clientId} authorizationParams={{
-            redirect_uri: redirectUrl
-        }}
-        onRedirectCallback={onRedirectCallback}>{children}</Auth0Provider>
-    )
-}
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: redirectUrl,
+      }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
 
 export default Auth0ProviderWithNavigate;
